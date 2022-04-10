507b6df6e28117f21bb60b105c1e5b25a9b0f572
LEGEN DARY MAD <tajay59@gmail.com>
2022-04-09 22:07:13 -0400

    Lab 7

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,6 +22,9 @@
             <li class="nav-item">
               <RouterLink class="nav-link" to="/about">About</RouterLink>
             </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/upload">Upload</RouterLink>
+            </li>
           </ul>
         </div>
       </div>
